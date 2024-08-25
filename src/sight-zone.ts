931e5a30c25dd3e9e6fff7e3dd03d0bacffa65d1
Anton Zotov <anton.a.zotov@gmail.com>
2024-08-25 14:21:24 +0200

    improve gradient

--- src/sight-zone.ts
+++ src/sight-zone.ts
@@ -4,14 +4,17 @@ export class SightZone implements Drawable {
     constructor(private readonly _rayA, private readonly _rayB) {}
 
     draw(ctx: CanvasRenderingContext2D): void {
-        const length = 1;
+        const normalLength = 100;
         const zoneEnd = {
             x: (this._rayA.end.x + this._rayB.end.x) / 2,
             y: (this._rayA.end.y + this._rayB.end.y) / 2,
         };
+        const length = Math.sqrt(
+            Math.pow(zoneEnd.x - this._rayA.start.x, 2) + Math.pow(zoneEnd.y - this._rayA.start.y, 2)
+        ) || 1;
         const normalizedEnd = {
-            x: zoneEnd.x + (zoneEnd.x - this._rayA.start.x) * length,
-            y: zoneEnd.y + (zoneEnd.y - this._rayA.start.y) * length,
+            x: zoneEnd.x + ((zoneEnd.x - this._rayA.start.x) / length) * normalLength,
+            y: zoneEnd.y + ((zoneEnd.y - this._rayA.start.y) / length) * normalLength,
         };
         const gradient = ctx.createLinearGradient(
             this._rayA.start.x,
